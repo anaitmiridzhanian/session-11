5b8e1c1b139adc83b49f6fd3f01a21b75cf45f54
Anait MIRIDZHANIAN <16473@PST8551.local>
2018-11-20 12:40:45 +0100

    Quizz Europe

---
+++ quiz.js
@@ -0,0 +1,32 @@
+var quiz = [
+  {
+    question: "Dans quel pays européen le Big Mac ne coûte que 1.54 dollar américain?",
+    choices: ["En Ukraine", "En Pologne"],
+    answer: 0
+  },
+  {
+    question: "Lequel de ces pays est le moins religieux ?",
+    choices: ["La Biélorussie", "L'Estonie"],
+    answer: 1
+  },
+  {
+    question: "Où ont été inventés les croissants?",
+    choices: ["En France", "En Autriche"],
+    answer: 1
+  },
+  {
+    question: "Dans quel pays on fume le plus par persone ?",
+    choices: ["En Andorre", "En Russie"],
+    answer: 0
+  },
+  {
+    question: "Où boit-on le plus de bière par habitant?",
+    choices: ["En Allemagne", "En République tchèque"],
+    answer: 1
+  },
+  {
+    question: "Dans quel pays les employés ont le plus d'heures de travail?",
+    choices: ["En Grèce", "En France"],
+    answer: 0
+  }
+];
